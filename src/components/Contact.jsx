0c8bd7424eef3ed2a7601ea1e5c44eec43223774
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2023-08-30 08:14:32 +0300

    load

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -124,6 +124,7 @@ const Contact = () => {
             className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
           >
             {loading ? "Sending..." : "Send"}
+            {setLoading(false)}
           </button>
         </form>
       </motion.div>
